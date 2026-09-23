59e0c245e26f2adb8a88c4a602a16fb09958beaf
vaughaunbruff-hub <vaughaunbruff@gmail.com>
2026-09-23 15:01:30 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -233,7 +233,7 @@ function sessionCookieOptions() {
       process.env.NODE_ENV === "production",
     sameSite: "lax",
     maxAge:
-      1000 * 60 * 60 * 24 * 7,
+  1000 * 60 * 10,
     path: "/"
   };
 }
